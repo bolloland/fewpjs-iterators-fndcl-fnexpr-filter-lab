bd824f64b41a36140111b0a13ed480c56d464728
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-08-23 23:54:29 -0500

    commit tree 64895c847cf0828516f4316ed85c2fadf1797992

--- index.js
+++ index.js
@@ -3,8 +3,13 @@
 function findMatching(driversArray, string) {
     return driversArray.filter(name => name.toLowerCase() === string.toLowerCase())
 }
-
+//CHARACTER MATCH
 function fuzzyMatch(drivers, string) {
-    let x = string.length
-    return drivers.filter(name => name.substring(0, x-1) === string)
+    return drivers.filter( name => {
+        return name.toLowerCase().indexOf(string.toLowerCase()) == 0
+    })
+}
+
+function matchName(drivers, string) {
+    return drivers.filter( obj => obj["name"].toLowerCase() === string.toLowerCase())
 }
