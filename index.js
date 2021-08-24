372ee00a872c2eb8bdf67a1c88d0aea92cbdc30b
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-08-23 23:18:09 -0500

    Done.

--- index.js
+++ index.js
@@ -4,4 +4,7 @@ function findMatching(driversArray, string) {
     return driversArray.filter(name => name.toLowerCase() === string.toLowerCase())
 }
 
-function fuzzyMatch(drivers, string)
+function fuzzyMatch(drivers, string) {
+    let x = string.length
+    return drivers.filter(name => name.substring(0, x-1) === string)
+}
